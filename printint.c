9206fddc6221bf9e5f5e738f5e5459f4690b3fad
iseltarek <iseltarek15@gmail.com>
2023-08-23 10:03:45 -0700

    solve errors

--- printint.c
+++ printint.c
@@ -1,81 +1,52 @@
 #include "main.h"
+/**
+ * printint - function that orint numbers
+ * @args:parameter
+ * Return: bytes
+*/
 int printint(va_list args)
 {
-        int num = va_arg(args, int), bytes = 0;
-        if (num == 0)
-        {
-                put('0');
-                return (1);
-        }
-        if (num < 0)
-        {
-                put('-');
-                num = num * -1;
-                bytes++;
-        }
-    bytes += pnums(num);
-    return (bytes);
+	int num = va_arg(args, int), bytes = 0;
 
+	if (num == 0)
+	{
+		put('0');
+		return (1);
+	}
+	if (num < 0)
+	{
+		put('-');
+		num = num * -1;
+		bytes++;
+	}
+	bytes += pnums(num);
+	return (bytes);
 }
+/**
+ * pnums - print digits
+ * @n: argument
+ * Return: 0
+*/
 int pnums(int n)
 {
-    int count_digit = 0, x = n;
-         int i, j;
-    char s;
-    while (x > 0)
-    {
-        x = x /10;
-        count_digit++;
-    }
- s = malloc(count_digit sizeof(char));
-    for (i = 0; i < count_digit; i++)
-    {
-        s[i] = (n  % 10 ) + '0';
-        n = n / 10;
-    }
-    for ( j = count_digit - 1; j >= 0; j-- )
-    {
-        put(s[j]);
-#include "main.h"
-int printint(va_list args)
-{
-        int num = va_arg(args, int), bytes = 0;
-        if (num == 0)
-        {
-                put('0');
-                return (1);
-        }
-        if (num < 0)
-        {
-                put('-');
-                num = num * -1;
-                bytes++;
-        }
-    bytes += pnums(num);
-    return (bytes);
+	int count_digit = 0, x = n;
+	int i, j;
+	char* s;
 
-}
-int pnums(int n)
-{
-    int count_digit = 0, x = n;
-         int i, j;
-    char s;
-    while (x > 0)
-    {
-        x = x /10;
-        count_digit++;
-    }
- s = malloc(count_digit sizeof(char));
-    for (i = 0; i < count_digit; i++)
-    {
-        s[i] = (n  % 10 ) + '0';
-        n = n / 10;
-    }
-    for ( j = count_digit - 1; j >= 0; j-- )
-    {
-        put(s[j]);
-}
-
-        return (0);
-
+	while (x > 0)
+	{
+		x = x / 10;
+		count_digit++;
+	}
+	s = malloc(count_digit *  sizeof(char));
+	for (i = 0; i < count_digit; i++)
+	{
+		s[i] = (n  % 10) + '0';
+		n = n / 10;
+	}
+	for (j = count_digit - 1; j >= 0; j--)
+	{
+		put(s[j]);
+	}
+	return (0);
 }
